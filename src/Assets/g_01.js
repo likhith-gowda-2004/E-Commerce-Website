a0bfcfadfdbc30c26a58977120201a0b98289d9e
likhith-gowda-2004 <likhithgowda2018@gmail.com>
2023-11-10 09:49:03 +0530

    message

---
+++ src/Assets/g_01.js
@@ -0,0 +1,353 @@
+import p200_img from "./product_200.jpg";
+import p201_img from "./product_201.jpg";
+import p202_img from "./product_202.jpg";
+//import p203_img from "./product_203.jpg";
+import p204_img from "./product_204.jpg";
+import p205_img from "./product_205.jpg";
+import p206_img from "./product_206.jpg";
+import p207_img from "./product_207.jpg";
+//import p208_img from "./product_208.jpg";
+import p209_img from "./product_209.jpg";
+import p210_img from "./product_210.jpg";
+import p211_img from "./product_211.jpg";
+import p212_img from "./product_212.jpg";
+import p213_img from "./product_213.jpg";
+import p214_img from "./product_214.jpg";
+import p215_img from "./product_215.jpg";
+import p216_img from "./product_216.jpg";
+import p217_img from "./product_217.jpg";
+import p218_img from "./product_218.jpg";
+import p219_img from "./product_219.jpg";
+import p220_img from "./product_220.jpg";
+import p221_img from "./product_221.jpg";
+import p222_img from "./product_222.jpg";
+import p223_img from "./product_223.jpg";
+import p224_img from "./product_224.jpg";
+import p225_img from "./product_225.jpg";
+import p226_img from "./product_226.jpg";
+//import p227_img from "./product_227.jpg";
+import p228_img from "./product_228.jpg";
+import p229_img from "./product_229.jpg";
+import p230_img from "./product_230.jpg";
+import p231_img from "./product_231.jpg";
+import p232_img from "./product_232.jpg";
+import p233_img from "./product_233.jpg";
+import p234_img from "./product_234.jpg";
+import p235_img from "./product_235.jpg";
+import p236_img from "./product_236.jpg";
+import p237_img from "./product_237.jpg";
+import p238_img from "./product_238.jpg";
+import p239_img from "./product_239.jpg";
+import p240_img from "./product_240.jpg";
+
+let grocery = [
+    {
+        id: 200,
+        name: "Item 1",
+        category: "grocery",
+        image: p200_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 201,
+        name: "Item 2",
+        category: "grocery",
+        image: p201_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 202,
+        name: "Item 3",
+        category: "grocery",
+        image: p202_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      
+
+      {
+        id: 204,
+        name: "Item 5",
+        category: "grocery",
+        image: p204_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 205,
+        name: "Item 6",
+        category: "grocery",
+        image: p205_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 206,
+        name: "Item 7",
+        category: "grocery",
+        image: p206_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 207,
+        name: "Item 8",
+        category: "grocery",
+        image: p207_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+     
+      {
+        id: 209,
+        name: "Item 10",
+        category: "grocery",
+        image: p209_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 210,
+        name: "Item 11",
+        category: "grocery",
+        image: p210_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 211,
+        name: "Item 12",
+        category: "grocery",
+        image: p211_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 212,
+        name: "Item 13",
+        category: "grocery",
+        image: p212_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 213,
+        name: "Item 14",
+        category: "grocery",
+        image: p213_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 214,
+        name: "Item 15",
+        category: "grocery",
+        image: p214_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 215,
+        name: "Item 16",
+        category: "grocery",
+        image: p215_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 216,
+        name: "Item 17",
+        category: "grocery",
+        image: p216_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 217,
+        name: "Item 18",
+        category: "grocery",
+        image: p217_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 218,
+        name: "Item 19",
+        category: "grocery",
+        image: p218_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 219,
+        name: "Item 20",
+        category: "grocery",
+        image: p219_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 220,
+        name: "Item 21",
+        category: "grocery",
+        image: p220_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 221,
+        name: "Item 22",
+        category: "grocery",
+        image: p221_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 222,
+        name: "Item 23",
+        category: "grocery",
+        image: p222_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 223,
+        name: "Item 24",
+        category: "grocery",
+        image: p223_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 224,
+        name: "Item 25",
+        category: "grocery",
+        image: p224_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 225,
+        name: "Item 26",
+        category: "grocery",
+        image: p225_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 226,
+        name: "Item 27",
+        category: "grocery",
+        image: p226_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      
+      {
+        id: 228,
+        name: "Item 29",
+        category: "grocery",
+        image: p228_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 229,
+        name: "Item 30",
+        category: "grocery",
+        image: p229_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 230,
+        name: "Item 31",
+        category: "grocery",
+        image: p230_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 231,
+        name: "Item 32",
+        category: "grocery",
+        image: p231_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 232,
+        name: "Item 33",
+        category: "grocery",
+        image: p232_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 233,
+        name: "Item 34",
+        category: "grocery",
+        image: p233_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 234,
+        name: "Item 35",
+        category: "grocery",
+        image: p234_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 235,
+        name: "Item 36",
+        category: "grocery",
+        image: p235_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 236,
+        name: "Item 37",
+        category: "grocery",
+        image: p236_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 237,
+        name: "Item 38",
+        category: "grocery",
+        image: p237_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 238,
+        name: "Item 39",
+        category: "grocery",
+        image: p238_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 239,
+        name: "Item 40",
+        category: "grocery",
+        image: p239_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+      {
+        id: 240,
+        name: "Item 41",
+        category: "grocery",
+        image: p240_img,
+        new_price: 300.0,
+        old_price: 400.5,
+      },
+]
+export default grocery
--- src/Pages/Products/grocery.js
+++ src/Pages/Products/grocery.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap'
-
+import Navbar from '../../Components/Navbar/Navbar';
+import './grocery.css';
+import Item from '../../Components/Item/Item';
+import grocery from '../../Assets/g_01';
 const Grocery = () => {
   return (
     <div>
         <Navbar/>
-        
+        <div className ="grocery">
+          <h1>
+            Today's deals
+          </h1>
+          <hr/>
+          
+          <div className = "collections">
+            {grocery.map((item,i)=>
+            {
+              return <Item key = {i} id = {grocery.id} name = {item.name} image ={item.image} new_price = {item.new_price} old_price ={item.old_price}/>
+            })}
+          </div>
+          
+        </div>
+    
       
     </div>
   )
