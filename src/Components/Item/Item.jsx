e9308a8894b92ef5291f90e0cdd602231ade0f50
likhith-gowda-2004 <likhithgowda2018@gmail.com>
2023-11-12 17:33:30 +0530

    Changes

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Item.css';
 import ShopIcon from '@mui/icons-material/Shop';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const Item = (props) => {
   const [show, setshow] = React.useState(false)
@@ -10,22 +11,27 @@ const Item = (props) => {
     
   }
   return (
-    <div className = 'item'>
-        <img src = {props.image} alt =""/>
-        <p>{props.name}</p>
-        <div className ="item-prices">
-            <div className="item-price-new">
-                ${props.new_price}
-            </div>
-            <div className="items-price-old">
-                {props.old_price}
-            </div>
-
+    <div className='item'>
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">
+          ${props.new_price}
         </div>
-      
+        <div className="item-price-old">
+          {props.old_price}
+        </div>
+        <div className="item-buttons">
+      <button className="add-to-cart-button">+</button>
+      <button className="buy-now-button"></button>
+       
+      </div>
+
+      </div>
+     
     </div>
-  )
-}
+  );
+};
 
 export default Item;
 
